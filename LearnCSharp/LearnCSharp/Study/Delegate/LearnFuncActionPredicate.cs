4902e66c3e2a26304a5e7b6abbc9c47d33ae0b2e
Muhammad Muso <musogulomov123@gmail.com>
2022-11-28 16:20:21 +0500

    STUDY: Learn Func , Action and Predicate.

---
+++ LearnCSharp/LearnCSharp/Study/Delegate/LearnFuncActionPredicate.cs
@@ -0,0 +1,43 @@
+﻿namespace LearnCSharp.Study.Delegate
+{
+    public class LearnFuncActionPredicate
+    {
+        public delegate double AreDelegate(int a, int b);
+        public static void main(string args)
+        {
+            AreDelegate areDelegate = new AreDelegate(Area);
+            areDelegate(3, 5);
+
+            Func<int, int, double> area = Area;
+            Console.WriteLine(area(3, 4));
+
+            Action<int, int> areaPrint = PrintArea;
+            areaPrint(5, 6);
+
+            // Func<int,bool> IsAreaBig = IsAreaBiggerThan100;
+            Predicate<int> isAreaBig = IsAreaBiggerThan100;
+            bool isBig = isAreaBig(10);
+            Console.WriteLine(isBig);
+
+            Func<int, double, string> circleArea = (radius, pi) => $"Area of the Circle is {pi * radius * radius}";
+
+            Console.WriteLine(circleArea(5,3.14));
+        }
+
+        public static double Area(int a, int b)
+        {
+            return a * b;
+        }
+        public static void PrintArea(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+        public static bool IsAreaBiggerThan100(int area)
+        {
+            return area > 100;
+        }
+
+
+    }
+}
